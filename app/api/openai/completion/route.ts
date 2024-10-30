de4284fa49995900f1600308e4dd853f1684d9d7
Mskeraao <michael7mancilla@gmail.com>
2024-10-30 15:12:16 +0800

    test maxDuration

--- app/api/openai/completion/route.ts
+++ app/api/openai/completion/route.ts
@@ -6,7 +6,7 @@ import OpenAI from "openai";
 
 // IMPORTANT! Set the runtime to edge
 //export const runtime = "edge";
-export const maxDuration = 60;
+export const maxDuration = 59;
 
 export async function POST(req: Request) {
   // Extract the `prompt` from the body of the request
